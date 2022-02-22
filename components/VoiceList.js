e2538b34f1a1b56e9a69c66207bf6e15debdcbf9
brandon <bradmccray89@gmail.com>
2022-02-21 21:50:42 -0500

    v1 of yoda web working

--- components/VoiceList.js
+++ components/VoiceList.js
@@ -1,38 +1,78 @@
 import AudioPlayer from './AudioPlayer';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import { useRouter } from 'next/router';
+import { getDatabase, ref, set } from 'firebase/database';
 
 export default function VoiceList({ voiceIntroList, currentUser }) {
 	const [currentVoiceIntro, setCurrentVoiceIntro] = useState(
 		currentUser.clipName || null
 	);
+	const router = useRouter();
 
-	console.log(currentVoiceIntro);
+	const refreshData = () => {
+		console.log(router.asPath);
+		router.reload(window.location.pathname);
+	};
+
+	const handleSave = () => {
+		const db = getDatabase();
+		set(ref(db, 'users/' + currentUser.id), {
+			id: currentUser.id,
+			clipName: currentVoiceIntro,
+		})
+			.then(() => {
+				refreshData();
+			})
+			.catch((error) => {
+				// The write failed...
+			});
+	};
 
 	return (
-		<div className="h-[32rem] w-[95%] sm:w-[80%] lg:w-[50rem] flex flex-col justify-start items-center rounded-lg border-2">
-			<h1 className="w-full text-2xl p-3 border-b-2">Voice Intros</h1>
-			{currentVoiceIntro && (
-				<ul className="h-full w-full m-3 flex flex-col justify-start items-center overflow-scroll scroll-smooth">
-					{voiceIntroList.map((voiceIntro) => (
-						<li
-							key={voiceIntro.name}
-							className="w-[95%] sm:w-[80%] mb-3 flex justify-center items-center"
-						>
-							<input
-								type="radio"
-								id={voiceIntro.name}
-								value={voiceIntro.name + voiceIntro.extension}
-								checked={currentVoiceIntro === voiceIntro.name + voiceIntro.extension}
-								onChange={(e) => {
-									setCurrentVoiceIntro(e.target.value);
-								}}
-								className="mr-5 h-5 w-5 cursor-pointer"
-							/>
-							<AudioPlayer voiceIntro={voiceIntro} />
-						</li>
-					))}
-				</ul>
+		<>
+			<div className="h-[32rem] w-[95%] sm:w-[80%] lg:w-[50rem] flex flex-col justify-start items-center rounded-lg border-2">
+				<h1 className="w-full text-2xl p-3 border-b-2">Voice Intros</h1>
+				{currentVoiceIntro && (
+					<ul className="h-full w-full m-3 flex flex-col justify-start items-center overflow-scroll scroll-smooth">
+						{voiceIntroList.map((voiceIntro) => (
+							<li
+								key={voiceIntro.name}
+								className="w-[95%] sm:w-[80%] mb-3 flex justify-center items-center"
+							>
+								<input
+									type="radio"
+									id={voiceIntro.name}
+									value={voiceIntro.name + voiceIntro.extension}
+									checked={currentVoiceIntro === voiceIntro.name + voiceIntro.extension}
+									onChange={(e) => {
+										setCurrentVoiceIntro(e.target.value);
+									}}
+									className="mr-5 h-5 w-5 cursor-pointer"
+								/>
+								<AudioPlayer voiceIntro={voiceIntro} />
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
+			{currentVoiceIntro && currentVoiceIntro !== currentUser.clipName && (
+				<div className="h-12 w-[95%] sm:w-[80%] lg:w-[50rem] flex place-content-stretch mt-3 gap-x-3">
+					<button
+						className="w-full h-full bg-gray-400 hover:bg-gray-500 text-white font-bold rounded-lg p-3"
+						onClick={() => {
+							setCurrentVoiceIntro(currentUser.clipName);
+						}}
+					>
+						Cancel
+					</button>
+					<button
+						className="w-full h-full bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg p-3"
+						onClick={handleSave}
+					>
+						Save
+					</button>
+				</div>
 			)}
-		</div>
+		</>
 	);
 }
